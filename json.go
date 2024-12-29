fdb77dde32288c05287bde1bf778202838af83ef
pssilv <pssilv@DESKTOP-9D96U41>
2024-12-29 20:42:09 -0300

    Formatted

--- json.go
+++ json.go
@@ -27,8 +27,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-  _, err = w.Write(dat)
-  if err != nil {
-    respondWithError(w, http.StatusInternalServerError, "Failed to write to the body")
-  }
+	_, err = w.Write(dat)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to write to the body")
+	}
 }
